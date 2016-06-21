d1b6bb5bbb756c2ca0124bd6cd48ecc89d106e87
anuprshetty <anuprshetty@gmail.com>
2016-06-21 20:39:07 +0530

    constructors

--- OOPs/class_n_object.py
+++ OOPs/class_n_object.py
@@ -1,20 +1,27 @@
 class Car:
     """This is a Car class"""
-    color = "red"
-    num = "123"
+    # Class attributes
+    count = 0
     def __init__(self, model_name, year):
+        Car.count = Car.count + 1
+        # Instance attributes
         self.model_name = model_name
         self.year = year
     
     def display(self):
         """This is a display method"""
-        print("{} - {}, {}, {}".format(self.model_name, self.year, self.color, self.num))
+        print("{} - {}, {}, {}".format(self.model_name, self.year, self.count, Car.count))
 
 
 car = Car("Benz", 2015)
 car.display()
 print(Car.__doc__)
 print(Car.display.__doc__)
+
+car2 = Car("Audi", 2017)
+Car.display(car2)
+print(Car.__dict__)
+print(car2.__dict__)
 
 # delete a property of the object
 del car.year
