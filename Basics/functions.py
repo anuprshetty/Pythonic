86a56f8db69a63e1b8a2a49adfc100984f5cc498
anuprshetty <anuprshetty@gmail.com>
2016-06-30 05:53:46 +0530

    functions

---
+++ Basics/functions.py
@@ -0,0 +1,33 @@
+# Order of execution
+def hello(): # 1. Create a Python variable hello which is a callable.
+    print("Hello World!") # 3. Execute the function.
+
+hello() # 2. Call the function.
+
+
+def add(x, y): # Parameters
+    # pass # Do Nothing
+    result = x + y
+    return result
+
+result = add(3, 5) # Positional Arguments
+print(result)
+
+result = add(y=2, x=4) # Keyword or named Arguments
+print(result)
+
+result = add(6, y=7) # Positional Arguments first, Keyword Arguments next.
+print(result)
+
+# Default parameter values in function.
+def subtract(x, y=3): # Required Parameters first, Optional/Default Parameters next.
+    # pass # Do Nothing
+    result = x - y
+    return result
+
+result = subtract(2)
+print(result)
+
+
+# NOTE: Avoid returning diffrent data types in a function, although it is possible. 
+# Best Practice - Always return same data type in a function.
